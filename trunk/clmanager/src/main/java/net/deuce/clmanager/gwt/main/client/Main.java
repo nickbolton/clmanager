62462ae110bd8a47226424077933ff1f74a59d0c
bolton.nick <bolton.nick@46464952-8c44-0410-a873-a3195b926a21>
2008-05-06 13:08:22 +0000

    removed unneccessary mygwt classes

--- trunk/clmanager/src/main/java/net/deuce/clmanager/gwt/main/client/Main.java
+++ trunk/clmanager/src/main/java/net/deuce/clmanager/gwt/main/client/Main.java
@@ -1,5 +1,7 @@
 package net.deuce.clmanager.gwt.main.client;
 
+import java.util.Date;
+
 import net.deuce.clmanager.gwt.main.client.model.UserModel;
 import net.deuce.clmanager.gwt.main.client.mvc.AppController;
 import net.deuce.clmanager.gwt.main.client.mvc.AppEvents;
@@ -8,15 +10,22 @@ import net.mygwt.ui.client.Registry;
 import net.mygwt.ui.client.mvc.AppEvent;
 import net.mygwt.ui.client.mvc.Dispatcher;
 import net.mygwt.ui.client.mvc.DispatcherListener;
-import asquare.gwt.debug.client.Debug;
-import asquare.gwt.debug.client.DebugConsole;
+import net.mygwt.ui.client.widget.Viewport;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.HistoryListener;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.gwtext.client.core.EventObject;
+import com.gwtext.client.core.Position;
+import com.gwtext.client.widgets.Button;
+import com.gwtext.client.widgets.event.ButtonListenerAdapter;
+import com.gwtext.client.widgets.event.KeyListener;
+import com.gwtext.client.widgets.form.FormPanel;
+import com.gwtext.client.widgets.form.TextField;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -29,13 +38,6 @@ public class Main implements EntryPoint, HistoryListener {
         DebugUtils.initDebugAndErrorHandling(20);
       //Registry.register("cl", createModel());
         
-        com.gwtext.client.widgets.Window debugWindow = new com.gwtext.client.widgets.Window("Debug Console", 550, 400, false, true);
-        debugWindow.add(DebugConsole.getInstance());
-        DebugConsole.getInstance().enable();
-        debugWindow.setMaximizable(true);
-        debugWindow.setMinimizable(true);
-        debugWindow.show();
-        Debug.println("Debugging...");
       
       dispatcher = Dispatcher.get();
       AppController appController = new AppController();
@@ -54,10 +56,21 @@ public class Main implements EntryPoint, HistoryListener {
         }
       });
       
+
+      String username = Cookies.getCookie("clmanager-user");
+      if (username == null) {
+          promptForUsername();
+      } else {
+          startApplication(username);
+      }
+
+    }
+    
+    private void startApplication(String username) {
       UserServiceAsync serviceProxy = (UserServiceAsync)GWT.create(UserService.class);
       ServiceDefTarget target = (ServiceDefTarget) serviceProxy;
       target.setServiceEntryPoint(GWT.getModuleBaseURL() + "UserService");
-      serviceProxy.getUser("deuce", new AsyncCallback() {
+      serviceProxy.getUser(username, new AsyncCallback() {
           public void onFailure (Throwable caught) { 
               Window.alert(DebugUtils.getStacktraceAsString(caught));
           } 
@@ -70,8 +83,59 @@ public class Main implements EntryPoint, HistoryListener {
               dispatcher.dispatch(AppEvents.Init);
           } 
       });
-
-
+    }
+    
+    private void promptForUsername() {
+        final com.gwtext.client.widgets.Window window = new com.gwtext.client.widgets.Window("Debug Console", 300, 130, false, true);
+        window.setClosable(false);
+        
+        FormPanel panel = new FormPanel();  
+        window.add(panel);
+        panel.setFrame(true);  
+        //panel.setFieldWidth(210);  
+        panel.setLabelWidth(100);  
+        panel.setButtonAlign(Position.CENTER);
+        panel.setWidth(400);  
+        
+        final TextField text = new TextField();  
+        text.setFieldLabel("Username");  
+        text.setEmptyText("Enter your username");  
+        text.setAllowBlank(false);  
+        text.setMinLength(4);  
+        text.addKeyListener(13, new KeyListener() {
+            public void onKey(int key, EventObject e) {
+                String username = text.getValueAsString().trim();
+                if (username.length() > 0) {
+                    Date aYearLater = new Date();
+                    aYearLater.setTime(aYearLater.getTime()+(365*24*3600000));
+                    
+                    Cookies.setCookie("clmanager-user", username, aYearLater);
+                    startApplication(username);
+                    window.close();
+                }
+            }
+        });
+        panel.add(text);  
+        
+        Button ok = new Button("Ok");
+        ok.addListener(new ButtonListenerAdapter() {
+            public void onClick(Button button, EventObject e) {
+                String username = text.getValueAsString().trim();
+                if (username.length() > 0) {
+                    Cookies.setCookie("clmanager-user", username);
+                    startApplication(username);
+                    window.close();
+                } else {
+                    e.stopEvent();
+                }
+            }
+        });
+        panel.addButton(ok);  
+        
+        Viewport vp = new Viewport();
+        vp.hideLoadingPanel("loading");
+        vp.dispose();
+        window.show();
     }
 
     public void onHistoryChanged(String historyToken) {
